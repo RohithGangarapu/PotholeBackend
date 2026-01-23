817ca0c499fe0f9da5ed79164082df2cd2d8e5e4
RohithGangarapu <gangarapurohith4@gmail.com>
2026-01-23 08:18:19 +0530

    postgres of render in settings.py added

--- backend/settings.py
+++ backend/settings.py
@@ -87,15 +87,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 import dj_database_url
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=True
+        default=os.environ.get("DATABASE_URL")
     )
 }
 # DATABASES = {
